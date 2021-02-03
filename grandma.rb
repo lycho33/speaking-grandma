f5b7aefe38cbd707bc7124cd99ed2812788bf348
lycho33 <cho.lyds@gmail.com>
2021-02-03 14:55:31 -0500

    commit tree 5a69987d22a7ea93e4d71787a38ed85a79226a96

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,15 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma(string)
+    if string == 'I LOVE YOU GRANDMA!'
+        return 'I LOVE YOU TOO PUMPKIN!'
+    elsif string == 'Hi Nana, how are you?'
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif string == 'Hi!'
+        return "HUH?! SPEAK UP, SONNY!"
+    else 
+        return "NO, NOT SINCE 1938!"
+    end
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
